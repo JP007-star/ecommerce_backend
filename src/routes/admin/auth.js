4daf4c666af074648231ed3f3d46a765f320c880
unknown <justice_of_peace@yahoo.com>
2022-08-08 01:04:44 +0530

    sign out added

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,13 +1,14 @@
 const express=require('express')
-const {signup, signin}=require('../../controller/admin/auth')
+const {signup, signin,signout}=require('../../controller/admin/auth')
 const { validateSignInRequest, isRequestValidated, validateSignUpRequest } = require('../../validators/auth')
+const {requireSigin} = require('../../middleware/index')
 const router=express.Router()
 
 
 
 router.post('/admin/signin',validateSignInRequest,isRequestValidated, signin)
-
 router.post('/admin/signup',validateSignUpRequest,isRequestValidated,signup)
+router.post('/admin/signout',requireSigin,signout)
 
 
 
